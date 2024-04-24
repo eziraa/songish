87699ef40d73b0d3a71291b624a521f4786d8892
eziraa <ezratgab@gmail.com>
2024-04-24 21:26:31 +0300

    Adding theme colors

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -1,23 +1,28 @@
-// src/styles/themes.js
 export type Theme = {
+  backgroundPrimary: string;
+  backgroundSecondary: string;
   body: string;
   text: string;
   toggleBorder: string;
-  background: string;
+  headerColor: string
 };
 
 export const lightTheme: Theme = {
+  backgroundPrimary: '#FEFEFE',
+  backgroundSecondary: '#E7E6E7',
   body: '#FFF',
   text: '#363537',
   toggleBorder: '#FFF',
-  background: '#363537',
+  headerColor: '#FFFFFF',
 };
 
 
 export const darkTheme:Theme = {
+  backgroundPrimary: '#091430',
+  backgroundSecondary: '#091D3A',
   body: '#363537',
   text: '#FAFAFA',
   toggleBorder: '#6B8096',
-  background: '#999',
+  headerColor: '#363537',
 };
 
