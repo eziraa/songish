c815d852b1ecc5b81b261be210bf6fda0c310ed7
eziraa <ezratgab@gmail.com>
2024-04-24 23:57:34 +0300

    Adding components of the menu bar

---
+++ src/components/sections/menu-bar/componets.style.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import { ThemeProps } from "../../../styles/theme-inteface";
+
+export const Menu = styled.div<ThemeProps>`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 15vw;
+  left: 0;
+  height: 100%;
+  background-color: ${({ theme }) => theme.backgroundSecondary};
+  color: ${({ theme }) => theme.text};
+  padding: 2rem;
+  position: fixed;
+  z-index: 10;
+`;
+export const MenuBar = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: start;
+`;
+
+export const MenuBarItem = styled.div<ThemeProps>`
+  background-color: transparent;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 12px 24px;
+  font-size: 0.9rem;
+  font-weight: bold;
+  max-width: 10rem;
+  border: 2px solid transparent;
+  cursor: pointer;
+  border-radius: 3px;
+  font-family: monospace;
+  transition: all 0.5s;
+  &:hover {
+    background-color: ${({ theme }) => theme.backgroundSecondaryHover};
+  }
+`;
