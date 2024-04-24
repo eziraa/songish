56b6ad45b3bd9386cab9c6bcb22bbde2a6bfbb42
eziraa <ezratgab@gmail.com>
2024-04-25 00:37:23 +0300

    updating the header style padding

--- src/components/sections/header/header.styles.tsx
+++ src/components/sections/header/header.styles.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { ThemeProps } from "../../../styles/theme-inteface";
 
 export const HeaderComponent = styled.div<ThemeProps>`
-  padding: 10px 10px;
+  padding: 15px 10px;
   padding-right: 2rem;
   display: flex;
   justify-content: end;
@@ -13,5 +13,4 @@ export const HeaderComponent = styled.div<ThemeProps>`
   top: 0;
   z-index: 1;
   background-color: ${({ theme }: ThemeProps) => theme.backgroundSecondary};
-  transition: all 0.5s linear;
 `;
