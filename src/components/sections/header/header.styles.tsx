5c2dbb97594357f226c267a6d2b1e96cec93ab85
eziraa <ezratgab@gmail.com>
2024-04-24 23:44:31 +0300

    styling the header

--- src/components/sections/header/header.styles.tsx
+++ src/components/sections/header/header.styles.tsx
@@ -2,14 +2,14 @@ import styled from "styled-components";
 import { ThemeProps } from "../../../styles/theme-inteface";
 
 export const HeaderComponent = styled.div<ThemeProps>`
-  padding: 10px 20px;
+  padding: 10px 10px;
+  padding-right: 2rem;
   display: flex;
   justify-content: end;
   align-items: center;
-  gap: 10rem;
   color: ${({ theme }) => theme.headerColor};
   position: fixed;
-  width: 98vw;
+  width: 100%;
   top: 0;
   z-index: 1;
   background-color: ${({ theme }: ThemeProps) => theme.backgroundSecondary};
