5171ea1aa55de0e81ea6f65af448fd86127dced8
eziraa <ezratgab@gmail.com>
2024-04-24 22:46:17 +0300

    styling the theme toggle btn

---
+++ src/components/buttons/theme-toggle/components.style.tsx
@@ -0,0 +1,24 @@
+import styled, { keyframes } from "styled-components";
+import { ThemeProps } from "../../../styles/theme-inteface";
+import { darkTheme, lightTheme } from "../../../styles/themes";
+
+export const LazyChange = keyframes<ThemeProps>`
+    0% {
+        background-color: ${({ theme }) =>
+          theme === lightTheme ? darkTheme.text : lightTheme.text};
+    }
+    100% {
+        background-color: ${({ theme }) =>
+          theme === lightTheme ? lightTheme.text : darkTheme.text};
+    }
+`;
+
+export const ToggleButton = styled.div<ThemeProps>`
+  font-size: 20px;
+  color: ${({ theme }) => theme.text};
+  transition: all 1.4s linear;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  cursor: pointer;
+`;
