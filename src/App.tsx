441e11113a105c67871251c1bdc87fa2dd00489a
eziraa <ezratgab@gmail.com>
2024-04-24 22:52:41 +0300

    updating the app file to provide the theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.js
+import { ThemeProvider } from "styled-components";
+import { ThemeContext, ThemeContextType } from "./contexts/theme_context";
+import { useTheme } from "./hooks/useTheme";
+import Header from "./components/sections/header/header";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [theme, toggleTheme] = useTheme(); // Update the destructuring assignment
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <ThemeContext.Provider
+      value={{ theme, toggleTheme } as unknown as ThemeContextType}
+    >
+      <ThemeProvider theme={theme}>
+        <Header />
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
 
-export default App
+export default App;
