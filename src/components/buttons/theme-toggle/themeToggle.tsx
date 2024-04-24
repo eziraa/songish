c1783a65d9613397c82295b1b805517392959aa2
eziraa <ezratgab@gmail.com>
2024-04-24 22:46:47 +0300

    creating a theme toggle btn

---
+++ src/components/buttons/theme-toggle/themeToggle.tsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import {
+  ThemeContext,
+  ThemeContextType,
+} from "../../../contexts/theme_context";
+import { lightTheme } from "../../../styles/themes";
+import { FaMoon } from "react-icons/fa";
+import { FiSun } from "react-icons/fi";
+import { ToggleButton } from "./components.style";
+export const ThemeToggleButton = () => {
+  const { theme, toggleTheme } = useContext<ThemeContextType>(ThemeContext);
+
+  return (
+    <ToggleButton onClick={toggleTheme}>
+      {theme === lightTheme ? <FiSun /> : <FaMoon />}
+    </ToggleButton>
+  );
+};
