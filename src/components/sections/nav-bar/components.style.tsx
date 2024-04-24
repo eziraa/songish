040f208babe4fe9a8bc2899048e54fecb34a1000
eziraa <ezratgab@gmail.com>
2024-04-24 23:48:52 +0300

    fixing the width problem

--- src/components/sections/nav-bar/components.style.tsx
+++ src/components/sections/nav-bar/components.style.tsx
@@ -7,7 +7,6 @@ export const UpperNavBar = styled.div<ThemeProps>`
   justify-content: end;
   color: ${({ theme }) => theme.text};
   justify-self: end;
-  min-width: 25vw;
   position: relative;
 `;
 
@@ -15,11 +14,12 @@ export const UpperNavBarItem = styled.div<ThemeProps>`
   padding: 8px 14px;
   font-size: 1.1rem;
   font-weight: bold;
+  min-width: 7rem;
   flex: 1;
   border: 2px solid transparent;
   cursor: pointer;
   border-radius: 3px;
-  font-family: monospace;
+  text-align: center;
   transition: all 0.5s;
   &:hover {
     background-color: ${({ theme }) => theme.backgroundSecondaryHover};
