ef793f70a2042fd1bd11a94999473a5b1f179125
eziraa <ezratgab@gmail.com>
2024-04-24 23:18:17 +0300

    adding more theme colors

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -5,24 +5,27 @@ export type Theme = {
   text: string;
   toggleBorder: string;
   headerColor: string
+  backgroundSecondaryHover: string
 };
 
 export const lightTheme: Theme = {
   backgroundPrimary: '#EAE6ED',
-  backgroundSecondary: '#C2C0C2',
+  backgroundSecondary: '#E7E7E7',
   body: '#FFF',
   text: '#010003',
   toggleBorder: '#FFF',
   headerColor: '#FFFFFF',
+  backgroundSecondaryHover: '#C2C0C2'
 };
 
 
 export const darkTheme:Theme = {
-  backgroundPrimary: '#091430',
+  backgroundPrimary: '#020d20',
   backgroundSecondary: '#091D3A',
-  body: '#363537',
+  body: '#020d20',
   text: '#F4F4F5',
   toggleBorder: '#6B8096',
   headerColor: '#363537',
+  backgroundSecondaryHover: '#183855'
 };
 
