3b4c791a3d0c73fbae3c9604b6633975a2b07377
eziraa <ezratgab@gmail.com>
2024-04-24 19:50:42 +0300

    Adding an interface to indicate the attribute of the theme object because fo the typescript is typed language

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -1,12 +1,20 @@
+// src/styles/themes.js
+export type Theme = {
+  body: string;
+  text: string;
+  toggleBorder: string;
+  background: string;
+};
 
-export const lightTheme = {
+export const lightTheme: Theme = {
   body: '#FFF',
   text: '#363537',
   toggleBorder: '#FFF',
   background: '#363537',
 };
 
-export const darkTheme = {
+
+export const darkTheme:Theme = {
   body: '#363537',
   text: '#FAFAFA',
   toggleBorder: '#6B8096',
