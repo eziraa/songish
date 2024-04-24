92ddabffe04ff9ab0ced3844be2f7f700079a276
eziraa <ezratgab@gmail.com>
2024-04-24 23:15:51 +0300

    adding the components of home page

---
+++ src/components/pages/homepage/components.style.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { ThemeProps } from "../../../styles/theme-inteface";
+
+export const Home = styled.div<ThemeProps>`
+  background-color: ${({ theme }) => theme.backgroundPrimary};
+  min-height: 100vh;
+  border: 2px solid transparent;
+`;
+export const Main = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
+  padding: 1rem;
+  left: 15vw;
+  padding-top: 20vh;
+  width: 80%;
+  gap: 2rem;
+  height: auto;
+`;
