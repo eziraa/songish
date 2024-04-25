4001e1ba6028c6dc3fdff13ae6467f2b9d9e7f56
eziraa <ezratgab@gmail.com>
2024-04-25 21:52:50 +0300

    Adding text theme colors

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -3,16 +3,20 @@ export type Theme = {
   backgroundSecondary: string;
   body: string;
   text: string;
+  textPrimary: string;
+  textSecondary: string;
   toggleBorder: string;
   headerColor: string
   backgroundSecondaryHover: string
 };
 
 export const lightTheme: Theme = {
-  backgroundPrimary: '#EAE6ED',
+  backgroundPrimary: '#FEFEFE',
   backgroundSecondary: '#E7E7E7',
   body: '#FFF',
   text: '#010003',
+  textPrimary: '#010003',
+  textSecondary: '#010003',
   toggleBorder: '#FFF',
   headerColor: '#FFFFFF',
   backgroundSecondaryHover: '#C2C0C2'
@@ -24,6 +28,8 @@ export const darkTheme:Theme = {
   backgroundSecondary: '#091D3A',
   body: '#020d20',
   text: '#F4F4F5',
+  textPrimary: '#F4F4F5',
+  textSecondary: '#F4F4F5',
   toggleBorder: '#6B8096',
   headerColor: '#363537',
   backgroundSecondaryHover: '#183855'
