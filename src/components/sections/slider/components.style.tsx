34d91cdbdf6d1d49148abb7b956ce8be92e9bcf6
eziraa <ezratgab@gmail.com>
2024-04-25 21:46:27 +0300

    Creating components of slider

---
+++ src/components/sections/slider/components.style.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const SliderBody = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  width: 80vw;
+  padding: 10px;
+`;
+export const Slide = styled.div`
+  height: 45vh;
+  width: 35vw;
+  display: flex;
+  align-items: center;
+  padding: 2rem;
+  margin: 0 1rem;
+  transition: transform 2s ease-out;
+`;
+
+export const SlidesContainer = styled.div`
+  width: 100%;
+  padding: 20px;
+  overflow-x: hidden;
+  display: flex;
+`;
