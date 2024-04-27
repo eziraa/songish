a77036c5af3477970c65be5a5188bde0131c11d6
eziraa <ezratgab@gmail.com>
2024-04-27 19:23:44 +0300

    defining interface to indicate the user response

---
+++ src/typo/user/response.tsx
@@ -0,0 +1,11 @@
+interface SongResponse {}
+interface PlaylistResponse {}
+export interface UserResponse {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  favorite_songs: SongResponse[];
+  my_songs: SongResponse[];
+  my_playlists: PlaylistResponse[];
+}
