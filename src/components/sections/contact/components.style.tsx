2eb672119ffe149a20ba0d2f7795e77c80efda26
eziraa <ezratgab@gmail.com>
2024-04-27 16:10:43 +0300

    adding the contact components

---
+++ src/components/sections/contact/components.style.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const ContactContainer = styled.div`
+  margin: 0 auto;
+  padding: 50px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 2rem;
+  color: #fff;
+  width: 80%;
+`;
