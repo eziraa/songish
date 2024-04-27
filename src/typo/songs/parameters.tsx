73a3fcb2115dfc9e7315442e7e34fc552e2d106a
eziraa <ezratgab@gmail.com>
2024-04-27 20:50:40 +0300

    creating interface to indicate the parameters use in operation up on the song

---
+++ src/typo/songs/parameters.tsx
@@ -0,0 +1,21 @@
+export interface LoadSongsParameters {
+  token: string;
+  playlistID: string;
+}
+export interface AddSongParams {
+  title?: string;
+  album?: string;
+  artist?: string;
+  duration?: number;
+  song_file?: any;
+  release_date?: string;
+  customer_id?: string;
+}
+export interface EditSongParams extends Omit<AddSongParams, "playlist_id"> {
+  id?: string;
+}
+
+export interface DeleteSongParams extends AddSongParams {
+  id: string;
+  callback?: () => void;
+}
