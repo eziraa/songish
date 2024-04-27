2efb6a368284b5afeab3671fba0be16130cb6d3e
eziraa <ezratgab@gmail.com>
2024-04-27 15:15:21 +0300

    Adding more search components

--- src/components/sections/search/components.style.tsx
+++ src/components/sections/search/components.style.tsx
@@ -1,5 +1,6 @@
-import styled from "@emotion/styled";
+import { styled } from "styled-components";
 import { ThemeProps } from "../../../styles/theme-inteface";
+import { Form } from "formik";
 
 export const SearchContainer = styled.div<ThemeProps>`
   display: flex;
@@ -8,6 +9,31 @@ export const SearchContainer = styled.div<ThemeProps>`
   background-color: ${({ theme }) => theme.headerColor};
   padding: 15px;
   border-radius: 5px;
+  &:focus {
+    outline: none;
+    border: none;
+  }
+`;
+
+export const SearchForm = styled(Form)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;
+export const SearchField = styled.input<ThemeProps>`
+  width: 300px;
+  font-size: 16px;
+  outline: none;
+  background-color: ${({ theme }) => theme.headerColor};
+  padding-left: 35px; /* Added padding to accommodate icon */
+  border: none;
+  color: white;
+  &::placeholder {
+    color: #aaa;
+  }
+
   &:focus {
     outline: none;
     border: none;
