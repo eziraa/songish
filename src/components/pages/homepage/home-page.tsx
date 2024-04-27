74108dcd6f33e6e11df01d7738fd0cea488062c9
eziraa <ezratgab@gmail.com>
2024-04-27 16:19:00 +0300

    trying to implement smooth scrolling

--- src/components/pages/homepage/home-page.tsx
+++ src/components/pages/homepage/home-page.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "../../sections/header/header";
 import NavBar from "../../sections/nav-bar/nav-bar";
 import { Home, Main } from "./components.style";
@@ -6,20 +5,33 @@ import LeftMenu from "../../sections/menu-bar/menu-bar";
 import { RecentSection } from "../../sections/recent-songs/recent";
 import AboutPage from "../../sections/about/about";
 import Footer from "../../sections/footer/footer";
-import { ScrollBar } from "../utils/scrollbar.style";
+import { ScrollBar } from "../../utils/scrollbar.style";
+import SignUpPage from "../../sections/sign_up/sign_up";
+import SearchComponent from "../../sections/search/search";
+import ContactPage from "../../sections/contact/contact";
+import { useRef } from "react";
 
 const HomePage = () => {
+  const homeRef = useRef<HTMLDivElement>(null);
+  const handleSmoothScroll = (nav: string) => {
+    homeRef.current?.querySelector(`#${nav}`)?.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
   return (
     <ScrollBar>
-      <Home>
+      <Home ref={homeRef}>
         <Header>
+          <SearchComponent />
           <NavBar />
         </Header>
         <LeftMenu />
         <Main>
           <RecentSection />
           <AboutPage />
+          <ContactPage />
           <Footer />
+          {/* <SignUpPage /> */}
         </Main>
       </Home>
     </ScrollBar>
