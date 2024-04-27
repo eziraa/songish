715bdd34ae0314c7b9b32122fce382f11b0af466
eziraa <ezratgab@gmail.com>
2024-04-27 20:56:33 +0300

    defining interface to indicate the song state

---
+++ src/typo/songs/state.tsx
@@ -0,0 +1,11 @@
+import { SongResponse } from "./response";
+
+export interface SongStateType {
+  deleting: boolean;
+  loading: boolean;
+  songs: SongResponse[];
+  current_song_for_action?: SongResponse;
+  current_song_to_play?: SongResponse;
+  query_set: SongResponse[];
+  playing_music_list: SongResponse[];
+}
