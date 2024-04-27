7d382c78e5b76b493915d5c260fe6da05d2a30fc
eziraa <ezratgab@gmail.com>
2024-04-27 15:21:19 +0300

    adding custom paragraph

---
+++ src/components/utils/paragraph.style.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Paragraph = styled.p`
+  text-align: start;
+  font-size: 1rem;
+  line-height: 1.3;
+  font-family: monospace;
+  font-weight: 500;
+  background-color: transparent;
+`;
