29ff22c565a505df108909d14d85662a64d09ad4
eziraa <ezratgab@gmail.com>
2024-04-27 19:24:31 +0300

    defining interface to indicate the user login and sign up parameters

---
+++ src/typo/user/parameters.tsx
@@ -0,0 +1,12 @@
+export interface LoginParameters {
+  email: string;
+  password: string;
+}
+
+export interface SignUpParameters {
+  fullName: string;
+  email: string;
+  gender: string;
+  password: string;
+  confirm_password: string;
+}
