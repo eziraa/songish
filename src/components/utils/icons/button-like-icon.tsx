76b343d0773ae20ccf5f42de4270b07a1cd445b4
eziraa <ezratgab@gmail.com>
2024-04-27 15:26:06 +0300

    Adding icons acts as btn

---
+++ src/components/utils/icons/button-like-icon.tsx
@@ -0,0 +1,20 @@
+import { FiSearch } from "react-icons/fi";
+import { HiMusicNote } from "react-icons/hi";
+import { MdFavorite } from "react-icons/md";
+import styled from "styled-components";
+import { ThemeProps } from "../../../styles/theme-inteface";
+
+export const SearchIcon = styled(FiSearch)<ThemeProps>`
+  position: absolute;
+  color: ${({ theme }) => theme.inputFieldColor};
+`;
+
+export const MusicIcon = styled(HiMusicNote)`
+  size: 20px;
+  color: #ef0a9b;
+`;
+
+export const Unfavorite = styled(MdFavorite)`
+  size: 20px;
+  color: #ef0a9b;
+`;
