382c6de86781c933d9712380003596648f17135e
eziraa <ezratgab@gmail.com>
2024-04-27 08:55:09 +0300

    Adding footer components

---
+++ src/components/sections/footer/components.style.tsx
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled";
+import { ThemeProps } from "../../../styles/theme-inteface";
+
+export const FooterContainer = styled.div<ThemeProps>`
+  background-color: transparent;
+  color: ${({ theme }) => theme.textSecondary};
+  padding: 20px;
+  text-align: center;
+  z-index: 200;
+`;
+
+export const FooterText = styled.p<ThemeProps>`
+  margin: 0;
+  font-size: 0.9rem;
+  color: ${({ theme }) => theme.textSecondary};
+`;
+
+export const FooterLink = styled.a`
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
