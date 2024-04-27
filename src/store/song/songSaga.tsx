ff5e1c251e3a32b1fd3affb155d0d51fa0a5f2fd
eziraa <ezratgab@gmail.com>
2024-04-27 21:19:15 +0300

    creating song saga and defining a method to load songs from the back end

---
+++ src/store/song/songSaga.tsx
@@ -0,0 +1,33 @@
+import { SagaReturnType, call, put } from "redux-saga/effects";
+import SongsAPI from "../../services/songAPI";
+import { setNotification } from "../notification/notificationSlice";
+import { loadSongsDone } from "./songSlice";
+
+function* LoadSongs() {
+  try {
+    const songs: SagaReturnType<typeof SongsAPI.loadSongs> = yield call(
+      SongsAPI.loadSongs
+    );
+
+    yield put(
+      setNotification({
+        color: "green",
+        status: true,
+        title: "Loading Song",
+        desc: "The songs successfully loaded",
+        duration: 3,
+      })
+    );
+    yield put(loadSongsDone(songs));
+  } catch (error) {
+    yield put(
+      setNotification({
+        color: "red",
+        status: true,
+        title: "Loading Song",
+        desc: "Can not load the songs from the database",
+        duration: 3,
+      })
+    );
+  }
+}
