44f76b492f02419552ad04d833856b3d32dff5e6
eziraa <ezratgab@gmail.com>
2024-04-27 20:07:13 +0300

    update the sign up saga to make the user logged in and to notify user he just signed up

--- src/store/user/userSaga.tsx
+++ src/store/user/userSaga.tsx
@@ -1,17 +1,39 @@
 import { PayloadAction } from "@reduxjs/toolkit";
 import { SignUpParameters } from "../../typo/user/parameters";
-import { SagaReturnType, call, takeEvery } from "redux-saga/effects";
+import { SagaReturnType, call, put, takeEvery } from "redux-saga/effects";
 import UserAPI from "../../services/useAPI";
-
+import { setNotification } from "../notification/notificationSlice";
+import { loginDone } from "./userSlice";
 function* SignUp(action: PayloadAction<SignUpParameters>) {
   try {
     let user: SagaReturnType<typeof UserAPI.signUp> = yield call(
       UserAPI.signUp,
       action.payload
     );
-  } catch (e) {}
+    yield put(loginDone(user));
+    yield put(
+      setNotification({
+        color: "green",
+        status: true,
+        title: "User sign up in",
+        desc: "You are successfully sign up in",
+        duration: 3,
+      })
+    );
+  } catch (e) {
+    if (e instanceof AxiosError) {
+      setNotification({
+        color: "green",
+        status: true,
+        title: "User sign up in",
+        desc: "Cannot register you",
+        duration: 3,
+      });
+    }
+  }
 }
-
 export function* watchSignup() {
   yield takeEvery("user/signUpRequest", SignUp);
 }
+
+
