073ac730ff77bd87c3c291f623a88c0469414660
eziraa <ezratgab@gmail.com>
2024-04-27 09:02:41 +0300

    wrapping the home page with custom scroll bar

--- src/components/pages/homepage/home-page.tsx
+++ src/components/pages/homepage/home-page.tsx
@@ -2,15 +2,27 @@ import React from "react";
 import Header from "../../sections/header/header";
 import NavBar from "../../sections/nav-bar/nav-bar";
 import { Home, Main } from "./components.style";
+import LeftMenu from "../../sections/menu-bar/menu-bar";
+import { RecentSection } from "../../sections/recent-songs/recent";
+import AboutPage from "../../sections/about/about";
+import Footer from "../../sections/footer/footer";
+import { ScrollBar } from "../utils/scrollbar.style";
 
 const HomePage = () => {
   return (
-    <Home>
-      <Header>
-        <NavBar />
-      </Header>
-      <Main />
-    </Home>
+    <ScrollBar>
+      <Home>
+        <Header>
+          <NavBar />
+        </Header>
+        <LeftMenu />
+        <Main>
+          <RecentSection />
+          <AboutPage />
+          <Footer />
+        </Main>
+      </Home>
+    </ScrollBar>
   );
 };
 
