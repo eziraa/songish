900af3aeca0c84cce008085ca6ca43c3ca64ad8b
eziraa <ezratgab@gmail.com>
2024-04-28 12:22:14 +0300

    creating interface to indicate the response of the playlist

---
+++ src/typo/playlist/state.tsx
@@ -0,0 +1,11 @@
+import { SongResponse } from "../songs/response";
+import { PlaylistResponse } from "./response";
+
+export interface PlaylistStateType {
+  deleting: boolean;
+  loading: boolean;
+  playlists: PlaylistResponse[];
+  currentPlaylist?: PlaylistResponse;
+  query_set?: PlaylistResponse[];
+  songs: SongResponse[];
+}
