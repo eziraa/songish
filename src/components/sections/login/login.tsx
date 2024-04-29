23e018311e0ef8757144bad82c13c5cfa900a929
eziraa <ezratgab@gmail.com>
2024-04-29 14:03:56 +0300

    updating the log in page to validate and to handle user login action

--- src/components/sections/login/login.tsx
+++ src/components/sections/login/login.tsx
@@ -1,21 +1,44 @@
-import {
-  Button,
-  Form,
-  FormGroup,
-  Input,
-  Label,
-  Title,
-} from "../../utils/form_field_elements.style";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { LoginPageContainer, LoginFormContainer } from "./components.style";
+import { LOGIN } from "../../../config/constants/user-current-task";
 import Modal from "../modal/modal";
-import { LoginFormContainer, LoginPageContainer } from "./components.style";
+import { useFormik, Form } from "formik";
+import { loginRequest } from "../../../store/user/userSlice";
+import { useAppSelector, useAppDispatch } from "../../../utils/customHook";
+import {
+  FormGroup,
+  Label,
+  Input,
+  Button,
+} from "../../utils/form_field_elements.style";
+import { Title } from "../recent-songs/components.style";
+import { loginSchema } from "../../../schema/user-auth/auth-schema";
 
 const LoginPage = () => {
+  const user = useAppSelector((state) => state.user);
+
+  const route = useNavigate();
+  const dispatch = useAppDispatch();
+  const formHandler = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: loginSchema,
+    onSubmit: async (values, actions) => {
+      dispatch(loginRequest(values));
+    },
+  });
+
+  if (user.minorTask !== LOGIN) return;
+
   return (
     <Modal>
       <LoginPageContainer>
         <LoginFormContainer>
           <Title style={{ color: "black" }}>Login</Title>
-          <Form>
+          <Form onSubmit={formHandler.handleSubmit}>
             <FormGroup>
               <Label htmlFor="email">Email</Label>
               <Input
@@ -23,6 +46,8 @@ const LoginPage = () => {
                 id="emails"
                 placeholder="Enter your email"
                 name="email"
+                onChange={formHandler.handleChange}
+                value={formHandler.values["email"]}
               />
             </FormGroup>
             <FormGroup>
@@ -32,6 +57,8 @@ const LoginPage = () => {
                 id="passwords"
                 placeholder="Enter your password"
                 name="password"
+                onChange={formHandler.handleChange}
+                value={formHandler.values["password"]}
               />
             </FormGroup>
             <Button type="submit">Login</Button>
