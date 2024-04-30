df9538dd8e0de081f99ff09871959ded6e00ec22
eziraa <ezratgab@gmail.com>
2024-04-30 20:13:47 +0300

    updating the modal to close it when the close btn is clicked and when the overlay clicked

--- src/components/sections/modal/modal.tsx
+++ src/components/sections/modal/modal.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { setMinorTask } from "../../../store/user/userSlice";
+import { useAppDispatch } from "../../../utils/customHook";
 import { ModalOverlay, ModalContent, CloseButton } from "./components.style";
 interface ModalProps {
   children: React.ReactNode;
 }
 const Modal = ({ children }: ModalProps) => {
+  const dispatch = useAppDispatch();
   return (
-    <ModalOverlay>
-      <ModalContent>
-        <CloseButton />
+    <ModalOverlay onClick={() => dispatch(setMinorTask(undefined))}>
+      <ModalContent onClick={(e) => e.stopPropagation()}>
+        <CloseButton onClick={() => dispatch(setMinorTask(undefined))} />
         {children}
       </ModalContent>
     </ModalOverlay>
