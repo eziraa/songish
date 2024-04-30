f3f3bb6843f5d0693249225b0f1bcef76176cff7
eziraa <ezratgab@gmail.com>
2024-04-30 20:17:42 +0300

    updating the router config to use the Router

--- src/config/router/router.tsx
+++ src/config/router/router.tsx
@@ -1,14 +1,12 @@
-import { createBrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GetStarted from "../../components/pages/get-started/get-started";
 import HomePage from "../../components/pages/homepage/home-page";
 
-export const routerConfig = createBrowserRouter([
-  {
-    path: "/",
-    element: <GetStarted />,
-  },
-  {
-    path: "/home",
-    element: <HomePage />,
-  },
-]);
+export const routerConfig = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<GetStarted />} />
+      <Route path="/home" element={<HomePage />} />
+    </Routes>
+  </Router>
+);
