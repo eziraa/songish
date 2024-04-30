29ba6e46e39906029d47fc2706fb1a285ba458fe
eziraa <ezratgab@gmail.com>
2024-04-30 23:11:06 +0300

    updating the song form to Add songs to the database

--- src/components/sections/song_add_update/song-form.tsx
+++ src/components/sections/song_add_update/song-form.tsx
@@ -1,3 +1,11 @@
+import { useFormik } from "formik";
+import { useAppDispatch, useAppSelector } from "../../../utils/customHook";
+import { AddingSongSchema } from "../../../schema/song-schema/songs";
+import { addSongRequested } from "../../../store/song/songSlice";
+import { UPLOAD_SONG } from "../../../config/constants/user-current-task";
+import Modal from "../modal/modal";
+import { AddSongContainer } from "./components.style";
+import { Description, Title } from "../about/components.style";
 import {
   Button,
   DescriptionText,
@@ -7,45 +15,126 @@ import {
   Input,
   Label,
 } from "../../utils/form_field_elements.style";
-import { Description, Title } from "../about/components.style";
-import Modal from "../modal/modal";
-import { AddSongContainer } from "./components.style";
+import { FormError } from "../sign_up/components.style";
+import SongsAPI from "../../../services/songAPI";
 
 const SongForm = () => {
+  const user = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
+  let isSubmitting = false;
+
+  const formHandler = useFormik({
+    initialValues: {
+      title: "",
+      artist: "",
+      album: "",
+      release_date: "",
+      customer_id: user.user.id,
+      duration: 0,
+      song_file: undefined,
+    },
+    validationSchema: AddingSongSchema,
+    onSubmit: (values) => {
+      isSubmitting = true;
+      dispatch(addSongRequested({ ...values, customer_id: user.user.id }));
+    },
+  });
+
+  if (user.minorTask != UPLOAD_SONG) return;
+
   return (
     <Modal>
       <AddSongContainer>
         <Description>
           <Title>Add Yours</Title>
           <DescriptionText>
-            Add your playlist local music and listen it on cool website"
+            Add your playlist local music and listen it on cool website
           </DescriptionText>
         </Description>
         <FormContainer>
-          <Form>
+          <Form onSubmit={formHandler.handleSubmit}>
             <FormGroup>
               <Label htmlFor="title">Title </Label>
-              <Input type="text" id="title" name="title" />
+              <Input
+                type="text"
+                id="title"
+                name="title"
+                onChange={formHandler.handleChange}
+                value={formHandler.values["title"]}
+              />
+              <FormError>
+                {formHandler.touched.title && formHandler.errors.title ? (
+                  <div>{formHandler.errors.title}</div>
+                ) : null}
+              </FormError>{" "}
             </FormGroup>
             <FormGroup>
               <Label htmlFor="artist">Artist</Label>
-              <Input type="text" id="artist" name="artist" />
+              <Input
+                type="text"
+                id="artist"
+                name="artist"
+                onChange={formHandler.handleChange}
+                value={formHandler.values.artist}
+              />
+              <FormError>
+                {formHandler.touched.artist && formHandler.errors.artist ? (
+                  <div>{formHandler.errors.artist}</div>
+                ) : null}
+              </FormError>{" "}
             </FormGroup>
             <FormGroup>
               <Label htmlFor="album">Album (Optional)</Label>
-              <Input type="text" id="album" name="album" />
+              <Input
+                type="text"
+                id="album"
+                name="album"
+                onChange={formHandler.handleChange}
+                value={formHandler.values.album}
+              />
             </FormGroup>
-            <Input type="hidden" id="duration" name="duration" />
+            <Input
+              type="hidden"
+              id="duration"
+              name="duration"
+              value={formHandler.values.duration}
+            />
             <FormGroup>
               <Label htmlFor="age">Release date (Optional) </Label>
-              <Input type="date" id="release_date" name="release_date" />
+              <Input
+                type="date"
+                id="release_date"
+                name="release_date"
+                onChange={formHandler.handleChange}
+                value={formHandler.values.release_date}
+              />
             </FormGroup>
 
             <FormGroup>
               <Label htmlFor="song_file">Audio</Label>
-              <Input type="file" id="song_file" name="song_file" />
+              <Input
+                type="file"
+                id="song_file"
+                name="song_file"
+                onChange={async (event) => {
+                  formHandler.setFieldValue(
+                    "song_file",
+                    event.currentTarget.files && event.currentTarget.files[0]
+                  );
+                  formHandler.setFieldValue(
+                    "duration",
+                    event.target.files &&
+                      (await SongsAPI.getAudioDuration(event.target.files[0]))
+                  );
+                }}
+              />
+              <FormError>
+                {formHandler.touched.song_file && (
+                  <>{formHandler.errors.song_file}</>
+                )}
+              </FormError>{" "}
             </FormGroup>
-            <Button type="submit">Add</Button>
+            <Button type="submit"> Add</Button>
           </Form>
         </FormContainer>
       </AddSongContainer>
