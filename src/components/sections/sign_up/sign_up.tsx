b6edb01f375ab0261bb25b1b11e9ec9bc9d8ef13
eziraa <ezratgab@gmail.com>
2024-04-30 10:46:19 +0300

    updating the sign up page to implement the sing up logic using formik package

--- src/components/sections/sign_up/sign_up.tsx
+++ src/components/sections/sign_up/sign_up.tsx
@@ -1,17 +1,45 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useFormik } from "formik";
+import { SignUpContainer, FormError } from "./components.style";
+import { SIGN_UP } from "../../../config/constants/user-current-task";
+import Modal from "../modal/modal";
 import {
+  FormContainer,
   Button,
+  Description,
   DescriptionText,
   Form,
-  FormContainer,
   FormGroup,
   Input,
   Label,
-} from "../../pages/utils/form_field_elements.style";
-import { Description, Title } from "../about/components.style";
-import Modal from "../modal/modal";
-import { SignUpContainer } from "./components.style";
+  Title,
+} from "../../utils/form_field_elements.style";
+import { useAppDispatch, useAppSelector } from "../../../utils/customHook";
+import { signUpSchema } from "../../../schema/user-auth/auth-schema";
+import { signUpRequest } from "../../../store/user/userSlice";
 
 const SignUpPage = () => {
+  const user = useAppSelector((state) => state.user);
+  //If the user does not want to sign up just return noting
+  const dispatch = useAppDispatch();
+
+  const formHandler = useFormik({
+    initialValues: {
+      fullName: "",
+      email: "",
+      gender: "",
+      password: "",
+      confirm_password: "",
+    },
+    validationSchema: signUpSchema,
+    onSubmit: (values, actions) => {
+      dispatch(signUpRequest(values));
+    },
+  });
+
+  if (user.minorTask !== SIGN_UP) return;
+
   return (
     <Modal>
       <SignUpContainer>
@@ -24,22 +52,70 @@ const SignUpPage = () => {
           </DescriptionText>
         </Description>
         <FormContainer>
-          <Form>
+          <Form
+            onSubmit={(e) => {
+              e.preventDefault();
+              formHandler.handleSubmit();
+            }}
+          >
             <FormGroup>
               <Label htmlFor="fullName">Full Name</Label>
-              <Input type="text" id="fullName" name="fullName" />
+              <Input
+                type="text"
+                id="fullName"
+                name="fullName"
+                value={formHandler.values.fullName}
+                onChange={formHandler.handleChange}
+              />
+              <FormError>
+                {formHandler.touched.fullName && formHandler.errors.fullName ? (
+                  <div>{formHandler.errors.fullName}</div>
+                ) : null}
+              </FormError>{" "}
             </FormGroup>
             <FormGroup>
               <Label htmlFor="gender">Gender</Label>
-              <Input type="text" id="age" name="gender" />
+              <Input
+                type="text"
+                id="age"
+                name="gender"
+                value={formHandler.values.gender}
+                onChange={formHandler.handleChange}
+              />
+              <FormError>
+                {formHandler.touched.gender && formHandler.errors.gender ? (
+                  <div>{formHandler.errors.gender}</div>
+                ) : null}
+              </FormError>{" "}
             </FormGroup>
             <FormGroup>
               <Label htmlFor="email">Email</Label>
-              <Input type="email" name="email" />
+              <Input
+                type="email"
+                name="email"
+                value={formHandler.values.email}
+                onChange={formHandler.handleChange}
+              />
+              <FormError>
+                {formHandler.touched.email && formHandler.errors.email ? (
+                  <div>{formHandler.errors.email}</div>
+                ) : null}
+              </FormError>{" "}
             </FormGroup>
             <FormGroup>
               <Label htmlFor="password">Password</Label>
-              <Input type="password" id="password" name="password" />
+              <Input
+                type="password"
+                id="password"
+                name="password"
+                value={formHandler.values.password}
+                onChange={formHandler.handleChange}
+              />
+              <FormError>
+                {formHandler.touched.password && formHandler.errors.password ? (
+                  <div>{formHandler.errors.password}</div>
+                ) : null}
+              </FormError>{" "}
             </FormGroup>
             <FormGroup>
               <Label htmlFor="confirm_password">Confirm Password</Label>
@@ -47,7 +123,15 @@ const SignUpPage = () => {
                 type="password"
                 id="confirm_password"
                 name="confirm_password"
+                value={formHandler.values.confirm_password}
+                onChange={formHandler.handleChange}
               />
+              <FormError>
+                {formHandler.touched.confirm_password &&
+                formHandler.errors.confirm_password ? (
+                  <div>{formHandler.errors.confirm_password}</div>
+                ) : null}
+              </FormError>{" "}
             </FormGroup>
 
             <Button type="submit"> submit</Button>
