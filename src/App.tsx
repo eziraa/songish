35a728d51249328d68fcc7b0dd490637123513c1
eziraa <ezratgab@gmail.com>
2024-04-30 20:14:58 +0300

    padding the store to the child of the app component by using the provider redux component

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,25 @@
 import { ThemeProvider } from "styled-components";
 import { ThemeContext, ThemeContextType } from "./contexts/theme_context";
 import { useTheme } from "./hooks/useTheme";
-import Header from "./components/sections/header/header";
+import HomePage from "./components/pages/homepage/home-page";
+import { GlobalStyles } from "./globalstyles/global-styles.style";
+import { Global } from "@emotion/react";
+import { Provider } from "react-redux";
+import { store } from "./utils";
 
 const App = () => {
   const [theme, toggleTheme] = useTheme(); // Update the destructuring assignment
   return (
-    <ThemeContext.Provider
-      value={{ theme, toggleTheme } as unknown as ThemeContextType}
-    >
-      <ThemeProvider theme={theme}>
-        <Header />
-      </ThemeProvider>
-    </ThemeContext.Provider>
+    <Provider store={store}>
+      <ThemeContext.Provider
+        value={{ theme, toggleTheme } as unknown as ThemeContextType}
+      >
+        <ThemeProvider theme={theme}>
+          <Global styles={GlobalStyles}></Global>
+          <HomePage />
+        </ThemeProvider>
+      </ThemeContext.Provider>
+    </Provider>
   );
 };
 
