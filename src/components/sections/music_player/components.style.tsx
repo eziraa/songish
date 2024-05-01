afa3148c08f05e213a498e787844d260894d0932
eziraa <ezratgab@gmail.com>
2024-05-01 23:17:16 +0300

    creating tracks and button container for the song player

--- src/components/sections/music_player/components.style.tsx
+++ src/components/sections/music_player/components.style.tsx
@@ -56,3 +56,37 @@ export const TrackArtist = styled.h3`
   font-size: 1.3rem;
   color: #fff;
 `;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const TrackStyles = css`
+  color: #fff;
+  margin: 20px;
+  padding: 5px 7px;
+  border-radius: 5px;
+`;
+export const RepeatTrack = styled.div`
+  ${TrackStyles}
+`;
+
+export const NextTrack = styled.div`
+  ${TrackStyles}
+`;
+
+export const PlayPauseTrack = styled.div`
+  ${TrackStyles}
+`;
+
+export const PrevTrack = styled.div`
+  ${TrackStyles};
+  width: 60%;
+`;
+
+export const RandomTrack = styled.div`
+  ${TrackStyles};
+  width: 30%;
+`;
