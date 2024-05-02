f266a301ceaabd1224a918f663f6fc00fbab36b6
eziraa <ezratgab@gmail.com>
2024-05-02 10:38:45 +0300

    Adding pop up to the song item to delete and update

--- src/components/sections/see_musics_list/display-muiscs.tsx
+++ src/components/sections/see_musics_list/display-muiscs.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../utils/customHook";
 import {
+  DeleteButton,
   MusicIcon,
   PlayPause,
+  PopUpContainer,
+  SongActions,
   SongAlbum,
   SongArtist,
   SongContainer,
+  SongDuration,
   SongHeader,
   SongInfo,
   SongInfoContainer,
@@ -13,16 +17,21 @@ import {
   SongTitle,
   SongsListTitle,
   SongsNotFound,
+  UpdateButton,
+  VerticalDots,
 } from "./components.style";
 import { SongResponse } from "../../../typo/songs/response";
 import LoadingSpinner from "../spinner/spinner";
 import { ScrollBar } from "../../utils/scrollbar.style";
 import { setCurrentSongToPlay } from "../../../store/song/songSlice";
+import { formatTime } from "../music_player/music-player";
+import { CloseButton } from "../modal/components.style";
 
 function MusicTable() {
   const songs = useAppSelector((state) => state.songs);
   const [song_list, setSongList] = useState<SongResponse[]>(songs.songs);
   const dispatch = useAppDispatch();
+  const [popUpIndex, setPopIndex] = useState(-1);
 
   useEffect(() => {
     setSongList(songs.songs);
@@ -50,9 +59,22 @@ function MusicTable() {
             <SongContainer key={index}>
               <SongMetaData>
                 <PlayPause
-                  onClick={() =>
-                    dispatch(setCurrentSongToPlay({ song, song_list }))
-                  }
+                  onClick={() => {
+                    dispatch(
+                      setCurrentSongToPlay({
+                        song: undefined,
+                        song_list: [],
+                      })
+                    );
+                    setTimeout(() => {
+                      dispatch(
+                        setCurrentSongToPlay({
+                          song,
+                          song_list,
+                        })
+                      );
+                    }, 1000);
+                  }}
                 />
                 <MusicIcon />
                 <SongInfoContainer>
@@ -63,6 +85,29 @@ function MusicTable() {
                   </SongInfo>
                 </SongInfoContainer>
               </SongMetaData>
+              <SongActions>
+                <SongDuration>{formatTime(song.duration || 0)} </SongDuration>
+
+                <div
+                  style={{
+                    position: "relative",
+                  }}
+                >
+                  <VerticalDots
+                    onClick={() => setPopIndex(index)}
+                  ></VerticalDots>
+                  {index === popUpIndex && (
+                    <PopUpContainer>
+                      <CloseButton
+                        style={{ top: "0", right: "0", color: "black" }}
+                        onClick={() => setPopIndex(-1)}
+                      />
+                      <DeleteButton>Delete</DeleteButton>
+                      <UpdateButton>Update</UpdateButton>
+                    </PopUpContainer>
+                  )}
+                </div>
+              </SongActions>
             </SongContainer>
           ))
         )}
