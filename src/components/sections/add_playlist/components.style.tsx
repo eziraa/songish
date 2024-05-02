40587bea16c7d70f945c9d0cb9931b0e2ae7af5e
eziraa <ezratgab@gmail.com>
2024-05-02 11:57:53 +0300

    creating components to Create playlist

---
+++ src/components/sections/add_playlist/components.style.tsx
@@ -0,0 +1,23 @@
+import { keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export const beVisible = keyframes`
+  from {
+    opacity: 0.6;
+    transform: skew(40deg);
+  }
+  to {
+    opacity: 1;
+    transform: skew(0deg);
+  }
+`;
+export const AddPlaylistContainer = styled.div`
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 60vw;
+  padding: 1rem;
+  padding-top: 50px;
+  animation: ${beVisible} 1.5s;
+`;
