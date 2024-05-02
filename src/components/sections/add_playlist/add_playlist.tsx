437d6361042ab9e12dc72e053da7654c3e79ca5b
eziraa <ezratgab@gmail.com>
2024-05-02 12:11:24 +0300

    updating the Playlist form to se the value of the input field and evaluate

--- src/components/sections/add_playlist/add_playlist.tsx
+++ src/components/sections/add_playlist/add_playlist.tsx
@@ -1,3 +1,6 @@
+import { useFormik } from "formik";
+import { CreatingPlaylistSchema } from "../../../schema/playlist-schema/playlist";
+import { useAppSelector } from "../../../utils/customHook";
 import {
   Button,
   Description,
@@ -11,33 +14,82 @@ import {
 } from "../../utils/form_field_elements.style";
 import Modal from "../modal/modal";
 import { AddPlaylistContainer } from "./components.style";
+import { CREATE_PLAYLIST } from "../../../config/constants/user-current-task";
+import { FormError } from "../sign_up/components.style";
 
 const PlaylistForm = () => {
+  const user = useAppSelector((state) => state.user);
+  const initialValues = {
+    name: "",
+    description: "",
+    image: undefined,
+    customer_id: user.user.id,
+  };
+  const formHandler = useFormik({
+    initialValues,
+    validationSchema: CreatingPlaylistSchema,
+    onSubmit: (values, actions) => {},
+  });
+
+  if (user.minorTask !== CREATE_PLAYLIST) return;
   return (
     <Modal>
       <AddPlaylistContainer>
         <Description>
           <Title>{"Create Your Playlist"}</Title>
           <DescriptionText>
-            Add your music local music and listen it on cool website
+            Create your own playlist and add songs to it
           </DescriptionText>
         </Description>
         <FormContainer>
-          <Form>
+          <Form onSubmit={formHandler.handleSubmit}>
             <FormGroup>
               <Label htmlFor="name">Playlist Name </Label>
-              <Input type="text" id="name" name="name" />
+              <Input
+                type="text"
+                id="name"
+                name="name"
+                onChange={formHandler.handleChange}
+                value={formHandler.values.name}
+              />
+              <FormError>
+                {formHandler.touched.name && formHandler.errors.name ? (
+                  <div>{formHandler.errors.name}</div>
+                ) : null}
+              </FormError>{" "}
             </FormGroup>
             <FormGroup>
               <Label htmlFor="description">Description</Label>
-              <Input type="text" id="description" name="description" />
+              <Input
+                type="text"
+                id="description"
+                name="description"
+                onChange={formHandler.handleChange}
+                value={formHandler.values.description}
+              />
             </FormGroup>
 
             <FormGroup>
               <Label htmlFor="image">Upload Image</Label>
-              <Input type="file" id="image" name="image" />
+              <Input
+                type="file"
+                id="image"
+                name="image"
+                onChange={async (event) => {
+                  formHandler.setFieldValue(
+                    "image",
+                    event.currentTarget.files && event.currentTarget.files[0]
+                  );
+                }}
+              />
+              <FormError>
+                {formHandler.touched.image && <>{formHandler.errors.image}</>}
+              </FormError>{" "}
             </FormGroup>
-            <Button type="submit">Add</Button>
+            <Button type="submit">
+              {" "}
+              {user.minorTask === CREATE_PLAYLIST ? "Add" : "Update"}{" "}
+            </Button>
           </Form>
         </FormContainer>
       </AddPlaylistContainer>
