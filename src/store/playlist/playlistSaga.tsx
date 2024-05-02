0ebb81e1a68257956030328bff85f7556ed7b879
eziraa <ezratgab@gmail.com>
2024-05-02 14:41:11 +0300

    Creating a saga middleware function and a binder method to add song playlist

--- src/store/playlist/playlistSaga.tsx
+++ src/store/playlist/playlistSaga.tsx
@@ -1,10 +1,17 @@
 import { PayloadAction } from "@reduxjs/toolkit";
 import { SagaReturnType, call, put, takeEvery } from "redux-saga/effects";
 import PlaylistsAPI from "../../services/playlistAPI";
-import { AddPlaylistParams } from "../../typo/playlist/parameters";
+import {
+  AddPlaylistParams,
+  AddSongToPlaylistParams,
+} from "../../typo/playlist/parameters";
 import { setNotification } from "../notification/notificationSlice";
 import { loadPlaylistsDone, setMinorTask } from "../user/userSlice";
-import { addPlaylistDone, loadingFinished } from "./playlistSlice";
+import {
+  addPlaylistDone,
+  addSongToPlaylistDone,
+  loadingFinished,
+} from "./playlistSlice";
 
 function* AddPlaylist(action: PayloadAction<AddPlaylistParams>) {
   try {
@@ -66,4 +73,36 @@ function* LoadPlaylists(action: PayloadAction<string>) {
 }
 export function* watchLoadPlaylists() {
   yield takeEvery("playlist/loadPlaylistsRequested", LoadPlaylists);
+}
+
+function* addSongToPlaylist(action: PayloadAction<AddSongToPlaylistParams>) {
+  try {
+    const songs: SagaReturnType<typeof PlaylistsAPI.addSongToPlayList> =
+      yield call(PlaylistsAPI.addSongToPlayList, action.payload);
+
+    yield put(
+      setNotification({
+        color: "green",
+        status: true,
+        title: "Adding song to Playlist",
+        desc: "The song successfully added to the playlist",
+        duration: 3,
+      })
+    );
+    yield put(addSongToPlaylistDone(songs));
+  } catch (error) {
+    yield put(
+      setNotification({
+        color: "red",
+        status: true,
+        title: "Adding  song to Playlist",
+        desc: "Cannot add song to Playlist",
+        duration: 3,
+      })
+    );
+  }
+}
+
+export function* watchAddSongToPlaylist() {
+  yield takeEvery("playlist/addSongToPlaylistRequested", addSongToPlaylist);
 }
