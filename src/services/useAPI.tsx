a1c3ab27b63a93d10e04a6bb07ebde1125c29b2a
eziraa <ezratgab@gmail.com>
2024-05-06 12:37:35 +0300

    Creating remote services to add songs to favorite

--- src/services/useAPI.tsx
+++ src/services/useAPI.tsx
@@ -1,7 +1,12 @@
 import axios from "axios";
-import { LoginParameters, SignUpParameters } from "../typo/user/parameters";
+import {
+  AddFavoriteSongsParams,
+  LoginParameters,
+  SignUpParameters,
+} from "../typo/user/parameters";
 import { UserResponse } from "../typo/user/response";
 import { api } from "../config/api/api";
+import { SongResponse } from "../typo/songs/response";
 
 const signUp = async (values: SignUpParameters) => {
   const user = await axios
@@ -15,6 +20,14 @@ const login = async ({ email, password }: LoginParameters) => {
     .post<UserResponse>(api + "/song/user/login", { email, password })
     .then((res) => res.data);
   return user;
+};
+const addFavoriteSongs = async (params: AddFavoriteSongsParams) => {
+  const response = await axios
+    .post<SongResponse>(
+      api + "/song/user/" + params.user_id + "/add_favorite/" + params.song_id
+    )
+    .then((res) => res.data);
+  return response;
 };
 const UserAPI = {
   signUp,
