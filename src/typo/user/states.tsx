daa89d265651556da54904d1da462ce616d20dc4
eziraa <ezratgab@gmail.com>
2024-05-06 13:11:11 +0300

    Adding user state property

--- src/typo/user/states.tsx
+++ src/typo/user/states.tsx
@@ -3,6 +3,7 @@ import { UserResponse } from "./response";
 export interface UserStateType {
   loading: boolean;
   user: UserResponse;
+  isOnAction: boolean;
   isLogin: boolean;
   majorTask: string | undefined;
   minorTask?: string | undefined;
