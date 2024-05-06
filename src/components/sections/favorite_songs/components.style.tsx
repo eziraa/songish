5075be7f8e29f31901acec5250c31bd430034074
eziraa <ezratgab@gmail.com>
2024-05-06 11:48:48 +0300

    Creating components for favorite songs section

---
+++ src/components/sections/favorite_songs/components.style.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import { OutLinedButton } from "../../utils/buttons.style";
+import { BtnContainer } from "../see_playlist/components.style";
+import { RecentContainer } from "../recent-songs/components.style";
+
+export const FavoriteBtn = styled(OutLinedButton)`
+  padding: 10px 20px;
+  color: white;
+`;
+
+export const FavoriteBtnContainer = styled(BtnContainer)`
+  display: flex;
+  width: 100%;
+  justify-content: start;
+  margin-left: 5rem;
+  align-items: center;
+`;
+
+export const FavoriteContainer = styled(RecentContainer)``;
