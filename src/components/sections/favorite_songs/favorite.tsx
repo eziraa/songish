af913536b443279759afe6b27c04c46898339536
eziraa <ezratgab@gmail.com>
2024-05-06 13:22:04 +0300

    updating the favorite song list to display songs in slider

--- src/components/sections/favorite_songs/favorite.tsx
+++ src/components/sections/favorite_songs/favorite.tsx
@@ -1,26 +1,64 @@
 import { useEffect, useState } from "react";
 import { H0 } from "../../utils/heading.style";
-import { Unfavorite } from "../../utils/icons/button-like-icon";
 import { Paragraph } from "../about/components.style";
 import { AboutMusic } from "../recent-songs/components.style";
 import { SliderBody, SlidesContainer, Slide } from "../slider/components.style";
-import {
-  FavoriteContainer,
-  FavoriteBtnContainer,
-  FavoriteBtn,
-} from "./components.style";
-import { useAppDispatch, useAppSelector } from "../../../utils/customHook";
+import { FavoriteContainer } from "./components.style";
+import { useAppSelector } from "../../../utils/customHook";
 import { SongResponse } from "../../../typo/songs/response";
 import { SEE_MY_FAVORITE_SONGS } from "../../../config/constants/user-current-task";
 import LoadingSpinner from "../spinner/spinner";
+import { BackIcon, ForwardIcon } from "../music_player/components.style";
 
 export const FavoriteSongs = () => {
   const user = useAppSelector((state) => state.user);
-  const dispatch = useAppDispatch();
+  const [prevIndex, setPrevIndex] = useState(0);
+  const [nextIndex, setNextIndex] = useState(1);
+  const [noPrev, setNoPrev] = useState(true);
+  const [noNext, setNoNext] = useState(false);
   const [song_list, setSongList] = useState<SongResponse[]>([]);
   useEffect(() => {
     setSongList(user.user.favorite_songs);
   }, [user.user, user.minorTask]);
+
+  useEffect(() => {
+    handleNextPrev();
+  }, [prevIndex, nextIndex]);
+  const handleNextPrev = () => {
+    if (nextIndex < (song_list ? song_list.length - 1 : 0)) {
+      setNoNext(false);
+    } else {
+      setNoNext(true);
+    }
+    if (prevIndex > 0) {
+      setNoPrev(false);
+    } else {
+      setNoPrev(true);
+    }
+  };
+  const nextSlide = () => {
+    if (song_list?.length || 0 > 0)
+      if (nextIndex + 2 < (song_list ? song_list.length : 0)) {
+        setNextIndex(nextIndex + 2);
+        setPrevIndex(prevIndex + 2);
+      } else if (nextIndex + 1 < (song_list ? song_list.length : 0)) {
+        setNextIndex(nextIndex + 1);
+        setPrevIndex(prevIndex + 1);
+      }
+  };
+
+  const prevSlide = () => {
+    if (song_list?.length || 0 > 0)
+      if (prevIndex - 2 >= 0) {
+        setNextIndex(nextIndex - 2);
+        setPrevIndex(prevIndex - 2);
+        handleNextPrev();
+      } else if (prevIndex - 1 >= 0) {
+        setNextIndex(nextIndex - 1);
+        setPrevIndex(prevIndex - 1);
+        handleNextPrev();
+      }
+  };
 
   if (user.majorTask !== SEE_MY_FAVORITE_SONGS) return;
   if (user.loading) {
@@ -30,15 +68,16 @@ export const FavoriteSongs = () => {
     <FavoriteContainer>
       <H0>Your favorite songs</H0>
       <SliderBody>
+        {!noPrev && <BackIcon onClick={() => prevSlide()} />}
         <SlidesContainer>
-          {song_list?.map((song, index, song_list) => {
+          {song_list?.map((music, index, music_list) => {
             if (true) {
               return (
                 <div key={index} style={{ transform: "translateX(34vw)" }}>
                   <Slide
                     key={index}
                     style={{
-                      transform: "translateX(" + -index * 37 + "vw)",
+                      transform: "translateX(" + -nextIndex * 37 + "vw)",
                       transition: "transform 2s ease-out",
                       backgroundImage:
                         "linear-gradient(rgba(34, 34, 34, 0.6),rgba(34, 34, 34, 0.6)),url(" +
@@ -48,17 +87,11 @@ export const FavoriteSongs = () => {
                     }}
                   >
                     <AboutMusic>
-                      <H0> {song.title} </H0>
+                      <H0> {music.title} </H0>
                       <Paragraph style={{ width: "20vw" }}>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Tempora mollitia saepe alias corrupti ut?
                       </Paragraph>
-                      <FavoriteBtnContainer>
-                        <FavoriteBtn>Listen now</FavoriteBtn>
-                        <FavoriteBtn>
-                          <Unfavorite /> Remove
-                        </FavoriteBtn>
-                      </FavoriteBtnContainer>
                     </AboutMusic>
                   </Slide>
                 </div>
@@ -66,6 +99,7 @@ export const FavoriteSongs = () => {
             }
           })}
         </SlidesContainer>
+        {!noNext && <ForwardIcon onClick={() => nextSlide()} />}
       </SliderBody>
     </FavoriteContainer>
   );
