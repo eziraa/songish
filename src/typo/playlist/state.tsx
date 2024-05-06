a95f94e2583133452488ae6246a6376d250dde3e
eziraa <ezratgab@gmail.com>
2024-05-06 11:19:09 +0300

    adding property to the playlist list state

--- src/typo/playlist/state.tsx
+++ src/typo/playlist/state.tsx
@@ -4,6 +4,7 @@ import { PlaylistResponse } from "./response";
 export interface PlaylistStateType {
   deleting: boolean;
   loading: boolean;
+  adding: boolean;
   playlists: PlaylistResponse[];
   currentPlaylist?: PlaylistResponse;
   query_set?: PlaylistResponse[];
