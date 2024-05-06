a959009fd3c12b6e9c28a3826088ae911c7ded08
eziraa <ezratgab@gmail.com>
2024-05-06 12:39:38 +0300

    Adding saga middleware to add songs to favorite

--- src/store/user/userSaga.tsx
+++ src/store/user/userSaga.tsx
@@ -1,9 +1,13 @@
 import { PayloadAction } from "@reduxjs/toolkit";
-import { LoginParameters, SignUpParameters } from "../../typo/user/parameters";
+import {
+  AddFavoriteSongsParams,
+  LoginParameters,
+  SignUpParameters,
+} from "../../typo/user/parameters";
 import { SagaReturnType, call, put, takeEvery } from "redux-saga/effects";
 import UserAPI from "../../services/useAPI";
 import { setNotification } from "../notification/notificationSlice";
-import { loginDone } from "./userSlice";
+import { addFavoriteSongDone, loginDone } from "./userSlice";
 function* SignUp(action: PayloadAction<SignUpParameters>) {
   try {
     let user: SagaReturnType<typeof UserAPI.signUp> = yield call(
@@ -33,8 +37,6 @@ function* SignUp(action: PayloadAction<SignUpParameters>) {
 export function* watchSignup() {
   yield takeEvery("user/signUpRequest", SignUp);
 }
-
-
 
 function* Login(action: PayloadAction<LoginParameters>) {
   try {
@@ -67,5 +69,37 @@ function* Login(action: PayloadAction<LoginParameters>) {
 
 export function* watchLogin() {
   yield takeEvery("user/loginRequest", Login);
+}
+
+function* addFavoriteSong(action: PayloadAction<AddFavoriteSongsParams>) {
+  try {
+    const response: SagaReturnType<typeof UserAPI.addFavoriteSongs> =
+      yield call(UserAPI.addFavoriteSongs, action.payload);
+
+    yield put(
+      setNotification({
+        color: "green",
+        status: true,
+        title: "Adding favorite song",
+        desc: "The song successfully added to your favorite list",
+        duration: 3,
+      })
+    );
+    yield put(addFavoriteSongDone(response));
+  } catch (error) {
+    yield put(
+      setNotification({
+        color: "red",
+        status: true,
+        title: "Adding favorite songs",
+        desc: "Cannot add this song to the favorite",
+        duration: 3,
+      })
+    );
+  }
+}
+
+export function* watchAddFavoriteSong() {
+  yield takeEvery("user/addFavoriteSongRequested", addFavoriteSong);
 }
 
