abc22473f2d3a278b79166cb2f3e3127126cf80e
eziraa <ezratgab@gmail.com>
2024-05-08 00:25:20 +0300

    updating the recent section to add icons

--- src/components/sections/recent-songs/recent.tsx
+++ src/components/sections/recent-songs/recent.tsx
@@ -1,16 +1,22 @@
 import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../utils/customHook";
 import { Slide, SliderBody, SlidesContainer } from "../slider/components.style";
-import { RecentContainer, AboutMusic, Title } from "./components.style";
+import {
+  RecentContainer,
+  AboutMusic,
+  SongsIcon,
+  IconisedTitle,
+} from "./components.style";
 import {
   loadSongsRequested,
   setCurrentSongToPlay,
 } from "../../../store/song/songSlice";
 import LoadingSpinner from "../spinner/spinner";
-import { H0 } from "../../utils/heading.style";
 import { BackIcon, ForwardIcon } from "../music_player/components.style";
-import { Paragraph } from "../../utils/paragraph.style";
-import { OutLinedButton } from "../../utils/buttons.style";
+import { Paragraph, PlayListBtn } from "../see_playlist/components.style";
+import { GiSpeaker } from "react-icons/gi";
+import { IoTimeOutline } from "react-icons/io5";
+import { H0 } from "../../utils/heading.style";
 
 export const RecentSection = () => {
   const songs = useAppSelector((state) => state.songs);
@@ -19,8 +25,6 @@ export const RecentSection = () => {
   const [nextIndex, setNextIndex] = useState(1);
   const [noPrev, setNoPrev] = useState(true);
   const [noNext, setNoNext] = useState(false);
-  const [currentPreview, setCurrentPreview] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     dispatch(loadSongsRequested());
@@ -70,7 +74,10 @@ export const RecentSection = () => {
   }
   return (
     <RecentContainer>
-      <H0>Resent musics</H0>
+      <IconisedTitle>
+        {" "}
+        <IoTimeOutline /> Recently added
+      </IconisedTitle>
       <SliderBody>
         {!noPrev && <BackIcon onClick={() => prevSlide()} />}
         <SlidesContainer>
@@ -84,19 +91,19 @@ export const RecentSection = () => {
                       transform: "translateX(" + -nextIndex * 37 + "vw)",
                       transition: "transform 2s ease-out",
                       backgroundImage:
-                        "linear-gradient(rgba(34, 34, 34, 0.6),rgba(34, 34, 34, 0.6)),url(" +
-                        `/src/assets/images/${index + 1}` +
-                        ".jpg)",
-                      backgroundSize: "cover",
+                        "linear-gradient(to right, #561e1ed5, #24249267)",
                     }}
                   >
                     <AboutMusic>
-                      <H0> {music.title} </H0>
+                      <H0 color="white"> {music.title} </H0>
                       <Paragraph style={{ width: "20vw" }}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Tempora mollitia saepe alias corrupti ut?
+                        Echoes of Now: Latest Tracks to Enchant Your Ears 🎵
                       </Paragraph>
-                      <OutLinedButton
+                      <PlayListBtn
+                        style={{
+                          color: "white",
+                          fontSize: "24px",
+                        }}
                         onClick={() => {
                           dispatch(
                             setCurrentSongToPlay({
@@ -106,9 +113,11 @@ export const RecentSection = () => {
                           );
                         }}
                       >
+                        <GiSpeaker />
                         Listen now
-                      </OutLinedButton>
+                      </PlayListBtn>
                     </AboutMusic>
+                    <SongsIcon />
                   </Slide>
                 </div>
               );
