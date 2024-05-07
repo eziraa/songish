208193d033c2d404ce90db27b81b07e99189baa2
eziraa <ezratgab@gmail.com>
2024-05-07 19:54:05 +0300

    Adding playlist icon

--- src/components/sections/see_playlist/components.style.tsx
+++ src/components/sections/see_playlist/components.style.tsx
@@ -1,3 +1,4 @@
+import { CgPlayList } from "react-icons/cg";
 import styled from "styled-components";
 
 export const PlaylistContainer = styled.div`
@@ -43,10 +44,18 @@ export const PlaylistInfo = styled.div`
   font-size: 20px;
   font-weight: bold;
   font-family: "Poppins", sans-serif;
+  z-index: 2;
   background-size: cover;
   height: 100%;
   border-radius: 1rem 0;
   @media (max-width: 768px) {
     flex-direction: column;
   }
+`;
+export const PlaylistIcon = styled(CgPlayList)`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  font-size: 150px;
+  color: #dd0a9e;
 `;
