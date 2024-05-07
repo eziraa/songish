302f4d24011a85f836722ca750d0860873ae1348
eziraa <ezratgab@gmail.com>
2024-05-08 01:06:04 +0300

    Adding more components

--- src/components/sections/favorite_songs/components.style.tsx
+++ src/components/sections/favorite_songs/components.style.tsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import { OutLinedButton } from "../../utils/buttons.style";
-import { BtnContainer } from "../see_playlist/components.style";
+import { BtnContainer, Paragraph } from "../see_playlist/components.style";
 import { RecentContainer } from "../recent-songs/components.style";
+import { TbMusicHeart } from "react-icons/tb";
+import { ThemeProps } from "../../../styles/theme-interface";
 
 export const Button = styled(OutLinedButton)`
   padding: 10px 20px;
@@ -10,10 +12,39 @@ export const Button = styled(OutLinedButton)`
 
 export const FavoriteBtnContainer = styled(BtnContainer)`
   display: flex;
-  width: 100%;
   justify-content: start;
-  margin-left: 5rem;
   align-items: center;
 `;
-
+export const FavSongIcon = styled(TbMusicHeart)`
+  font-size: 150px;
+  color: ${({ theme }) => theme.textSecondary};
+`;
 export const FavoriteContainer = styled(RecentContainer)``;
+
+export const FavParagraph = styled(Paragraph)`
+  background-image: linear-gradient(
+    to top right,
+
+    #ffa600,
+    #f92cdb,
+    #fdfcfe,
+    #ffa600,
+    #f92cdb,
+    #fefdfe
+  );
+`;
+
+export const DeleteButton = styled.button<ThemeProps>`
+  background-color: transparent;
+  border: 1px solid #ff00c8;
+  cursor: pointer;
+  color: white;
+  font-size: 1.1rem;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  &:hover {
+    background-color: red;
+    color: ${({ theme }) => theme.formBtnTextColor};
+  }
+`;
