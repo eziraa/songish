f62fe29a9989eea0034a7fd6b493e1bf754d5075
eziraa <ezratgab@gmail.com>
2024-05-08 07:15:09 +0300

    Adding overlay for the playlist card

--- src/components/sections/see_playlist/playlist.tsx
+++ src/components/sections/see_playlist/playlist.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { PlaylistResponse } from "../../../typo/playlist/response";
 import { timeAgo } from "../../utils/time_ago";
 import { ThemeProps } from "../../../styles/theme-interface";
@@ -11,6 +11,8 @@ import {
   ModalContent,
   ModalOverlay,
 } from "../modal/components.style";
+import { CgPlayListAdd } from "react-icons/cg";
+import { MdPlaylistAddCheck } from "react-icons/md";
 interface ModalProps {
   children: React.ReactNode;
   length: number;
@@ -21,7 +23,7 @@ const Modal = ({ children, length }: ModalProps) => {
     <ModalOverlay onClick={() => dispatch(setMinorTask(undefined))}>
       <ModalContent
         onClick={(e) => e.stopPropagation()}
-        style={{ width: Math.ceil(Math.sqrt(length)) * 240 + "px" }}
+        style={{ width: Math.ceil(Math.sqrt(length)) * 270 + "px" }}
       >
         <CloseButton onClick={() => dispatch(setMinorTask(undefined))} />
         {children}
@@ -31,14 +33,50 @@ const Modal = ({ children, length }: ModalProps) => {
 };
 
 const CardContainer = styled.div<ThemeProps>`
+  position: relative;
   display: flex;
   border-radius: 10px 0;
   margin: 10px;
   padding: 20px;
-  width: 200px;
+  width: 230px;
   background-color: ${({ theme }) => theme.backgroundSecondary};
+  cursor: pointer;
+  overflow: hidden; // Add this line
+  &:hover .overlay {
+    opacity: 1;
+  }
 `;
 
+const slideDown = keyframes`
+  0% {
+    transform: translateY(150%);
+  }
+  100% {
+    transform: translateY(0%);
+    height: 100%;
+  }
+`;
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.648);
+  color: #f00ba4;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  opacity: 0;
+  transition: opacity 1s ease;
+  font-size: large;
+  ${CardContainer}:hover & {
+    top: 0;
+    opacity: 1;
+    animation: ${slideDown} 1s;
+  }
+`;
 const PlaylistName = styled.h2`
   margin: 0;
 `;
@@ -78,7 +116,11 @@ const PlaylistCard = ({ playlist }: { playlist: PlaylistResponse }) => (
       <PlaylistName>{playlist.name}</PlaylistName>
       <TimeCreated>{timeAgo(playlist.created_at)}</TimeCreated>
     </Card>
-    <PlaylistIcon size={40} />
+    <PlaylistIcon size={60} />
+    <Overlay className="overlay">
+      Add to this playlist
+      <MdPlaylistAddCheck size={40} />
+    </Overlay>
   </CardContainer>
 );
 
@@ -87,6 +129,16 @@ const Playlists = ({ playlists }: { playlists: PlaylistResponse[] }) => (
     {playlists.map((playlist) => (
       <PlaylistCard key={playlist.id} playlist={playlist} />
     ))}
+    <CardContainer>
+      <Card>
+        <PlaylistName> Create New Playlist</PlaylistName>
+      </Card>
+      <CgPlayListAdd size={60} />
+      <Overlay className="overlay">
+        Create New Playlist
+        <CgPlayListAdd size={40} />
+      </Overlay>
+    </CardContainer>
   </Modal>
 );
 
