1a2e065d8494d3e08d0c6ba9df531464a18117a4
eziraa <ezratgab@gmail.com>
2024-05-08 14:27:34 +0300

    updating the about section

--- src/components/sections/about/about.tsx
+++ src/components/sections/about/about.tsx
@@ -10,63 +10,54 @@ import {
   FeatureItemBody,
   FeatureItem,
   Image,
+  ImageSong,
 } from "./components.style";
 
 const AboutPage = () => {
   return (
     <AboutContainer id="about">
       <TitleBackGround>
-        <Title>About my Music website</Title>
+        <Title>About me</Title>
       </TitleBackGround>
       <DescriptionContainer>
         <TitleSmall>What is it? `</TitleSmall>
         <Description>
-          <Image src="src/assets/images/rmg.png" />
+          <Image
+            style={{ borderRadius: "50%" }}
+            src="src/assets/images/ezira.jpg"
+          />
           <Paragraph>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad,
-            officiis! Aut, impedit. Dolore tempore tempora, reprehenderit est
-            dolorum dicta iusto esse quisquam labore, recusandae vero! Debitis
-            ut cum ea provident.
+            Hello! I'm a 4th year Software Engineering student at Bahir Dar
+            University. I'm currently exploring full-stack development and have
+            a keen interest in creating dynamic and user-friendly applications.
           </Paragraph>
         </Description>
       </DescriptionContainer>
       <FeatureList>
         <FeatureItemBody>
           <FeatureItem>
-            <TitleSmall>Feature 1</TitleSmall>
+            <TitleSmall>Skills</TitleSmall>
             <Paragraph>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad,
-              officiis! Aut, impedit. Dolore tempore tempora, reprehenderit est
-              dolorum dicta iusto esse quisquam labore, recusandae vero! Debitis
-              ut cum ea provident.
+              I have experience with a variety of programming languages
+              including C++, JavaScript, Java, and Python. I'm also familiar
+              with several frameworks and libraries such as Bootstrap, Tailwind
+              CSS, Styled Components, React, and Django.
             </Paragraph>
           </FeatureItem>
-          <Image src="src/assets/images/rmg.png" />
+          <Image src="src/assets/images/languages.webp" />
         </FeatureItemBody>
         <FeatureItemBody>
-          <Image src="src/assets/images/rmg.png" />
+          <ImageSong></ImageSong>
 
           <FeatureItem>
-            <TitleSmall>Feature 1</TitleSmall>
+            <TitleSmall>About the app</TitleSmall>
             <Paragraph>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad,
-              officiis! Aut, impedit. Dolore tempore tempora, reprehenderit est
-              dolorum dicta iusto esse quisquam labore, recusandae vero! Debitis
-              ut cum ea provident.
+              This song app is a testament to my learning journey. It's built
+              using React for the frontend, Django for the backend, and Styled
+              Components for styling. It's a simple yet functional music player
+              that allows you to enjoy your favorite tunes anytime, anywhere.
             </Paragraph>
           </FeatureItem>
-        </FeatureItemBody>
-        <FeatureItemBody>
-          <FeatureItem>
-            <TitleSmall>Feature 1</TitleSmall>
-            <Paragraph>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad,
-              officiis! Aut, impedit. Dolore tempore tempora, reprehenderit est
-              dolorum dicta iusto esse quisquam labore, recusandae vero! Debitis
-              ut cum ea provident.
-            </Paragraph>
-          </FeatureItem>
-          <Image src="src/assets/images/rmg.png" />
         </FeatureItemBody>
       </FeatureList>
     </AboutContainer>
