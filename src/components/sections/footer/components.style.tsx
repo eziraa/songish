1da3c43ec286f4592f76208db0ee3b6a15605b87
eziraa <ezratgab@gmail.com>
2024-05-08 16:32:21 +0300

    updating the footer components

--- src/components/sections/footer/components.style.tsx
+++ src/components/sections/footer/components.style.tsx
@@ -4,16 +4,35 @@ import { ThemeProps } from "../../../styles/theme-interface";
 export const FooterContainer = styled.div<ThemeProps>`
   background-color: transparent;
   color: ${({ theme }) => theme.textSecondary};
-  padding: 20px;
   text-align: center;
   z-index: 200;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-left: -10rem;
+  margin-bottom: -2rem;
+  width: 100vw;
+  padding: 1rem 5rem;
+  background-color: ${({ theme }) => theme.backgroundSecondary};
 `;
 
 export const FooterText = styled.p<ThemeProps>`
   margin: 0;
   font-size: 0.9rem;
-  color: ${({ theme }) => theme.textSecondary};
+  color: ${({ theme }) => theme.footerTextColor};
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+  line-height: 1.5;
+  letter-spacing: 1px;
+  text-align: start;
 `;
+
+export const CopyRight = styled(FooterText)`
+  font-size: 1.2rem;
+  font-weight: 400;
+  color: ${({ theme }) => theme.footerTextColor};
+`;
+
 
 export const FooterLink = styled.a`
   text-decoration: none;
