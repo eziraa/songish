932c58d35cba2dec2bba74694a3ff7aac2fa43d1
eziraa <ezratgab@gmail.com>
2024-05-08 16:08:32 +0300

    updating contact page

--- src/components/sections/contact/contact.tsx
+++ src/components/sections/contact/contact.tsx
@@ -9,36 +9,38 @@ import {
   Form,
 } from "../../utils/form_field_elements.style";
 import { Description } from "../about/components.style";
-import { Title } from "../recent-songs/components.style";
-import { ContactContainer } from "./components.style";
+import { ContactBody, ContactContainer, Title } from "./components.style";
 
 const ContactPage = () => {
   return (
     <ContactContainer id="contact">
-      <Description>
-        <Title>Contact Us</Title>
-        <DescriptionText>
-          Have any questions, suggestions, or just want to say hello? Feel free
-          to drop us a message using the form below. We'd love to hear from you!
-        </DescriptionText>
-      </Description>
-      <FormContainer>
-        <Form>
-          <FormGroup>
-            <Label htmlFor="name">Your Name</Label>
-            <Input type="text" id="name" />
-          </FormGroup>
-          <FormGroup>
-            <Label htmlFor="email">Your Email</Label>
-            <Input type="email" id="email" />
-          </FormGroup>
-          <FormGroup>
-            <Label htmlFor="message">Your Message</Label>
-            <TextArea id="message" rows="6" />
-          </FormGroup>
-          <Button type="submit">Send Message</Button>
-        </Form>
-      </FormContainer>
+      <Title>Contact Us</Title>
+      <ContactBody>
+        <Description>
+          <DescriptionText>
+            Have any questions, suggestions, or just want to say hello? Feel
+            free to drop me a message using the form below. I'd love to hear
+            from you!
+          </DescriptionText>
+        </Description>
+        <FormContainer>
+          <Form>
+            <FormGroup>
+              <Label htmlFor="name">Your Name</Label>
+              <Input type="text" id="name" />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="email">Your Email</Label>
+              <Input type="email" id="email" />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="message">Your Message</Label>
+              <TextArea id="message" rows="6" />
+            </FormGroup>
+            <Button type="submit">Send Message</Button>
+          </Form>
+        </FormContainer>
+      </ContactBody>
     </ContactContainer>
   );
 };
