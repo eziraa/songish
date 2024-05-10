d5d6f1d3563d0917c759cd77724d739f3e9284fb
eziraa <ezratgab@gmail.com>
2024-05-10 22:00:06 +0300

    Creating new modal t

--- src/components/sections/login/components.style.tsx
+++ src/components/sections/login/components.style.tsx
@@ -11,7 +11,7 @@ export const LoginPageContainer = styled.div`
 `;
 
 export const LoginFormContainer = styled.div`
-  width: 40rem;
+  width: 30rem;
   border-radius: 1rem;
   padding: 3.2rem;
   box-shadow: 0 0.4rem 0.8rem rgba(0, 0, 0, 0.1);
@@ -19,3 +19,27 @@ export const LoginFormContainer = styled.div`
   flex-direction: column;
   gap: 0.8rem;
 `;
+
+export const ModalTitle = styled.div`
+  font-size: 1.6rem;
+  font-weight: 600;
+  font-family: "Roboto";
+  color: transparent;
+  font-size: 2.5rem;
+  position: relative;
+  text-align: start;
+  width: 13rem;
+  background-clip: text;
+  background-image: linear-gradient(to right, #14beaa, white);
+  &::after {
+    content: "";
+    width: 100%;
+    top: 0.4rem;
+    left: 0;
+    height: 0.5rem;
+    background: linear-gradient(to right, #14beaa, white);
+    margin-top: 2.6rem;
+    position: absolute;
+  }
+`;
+  
