1fce6b6ae25ab6429d07c7a1925ea2b942c9737b
eziraa <ezratgab@gmail.com>
2024-05-12 23:34:47 +0300

    updating the toggle btn for its event do not propagate

--- src/components/buttons/theme-toggle/themeToggle.tsx
+++ src/components/buttons/theme-toggle/themeToggle.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import {
   ThemeContext,
   ThemeContextType,
@@ -7,11 +7,17 @@ import { lightTheme } from "../../../styles/themes";
 import { FaMoon } from "react-icons/fa";
 import { FiSun } from "react-icons/fi";
 import { ToggleButton } from "./components.style";
+
 export const ThemeToggleButton = () => {
   const { theme, toggleTheme } = useContext<ThemeContextType>(ThemeContext);
-
   return (
-    <ToggleButton onClick={toggleTheme}>
+    <ToggleButton
+      onClick={(e) => {
+        e.stopPropagation();
+        toggleTheme();
+      }}
+      style={{ border: "2px solid red" }}
+    >
       {theme === lightTheme ? <FiSun /> : <FaMoon />}
     </ToggleButton>
   );
